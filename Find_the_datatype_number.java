0eb1fda35f321be92b6713e46e88bcefa18beed2
Madhan200467 <2k22eee20@kiot.ac.in>
2025-09-11 19:30:33 +0530

    Create Find_the_datatype_number.java

---
+++ Find_the_datatype_number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)throws InterruptedException {
+		int a=3;
+		double b=3.5;
+		if(a%1==0)
+		System.out.println("int");
+		else
+		System.out.println("float");
+		if(b%1==0)
+		System.out.println("int");
+		else
+		System.out.println("float");
+	}
+}
