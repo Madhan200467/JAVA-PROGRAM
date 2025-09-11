06362809871a8e9ae1bf6f89aee910399669a00d
Madhan200467 <2k22eee20@kiot.ac.in>
2025-09-11 19:38:03 +0530

    Update Find_the_datatype_number.java

--- Find_the_datatype_number.java
+++ Find_the_datatype_number.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main
 {
-	public static void main(String[] args)throws InterruptedException {
+	public static void main(String[] args){
 		int a=3;
 		double b=3.5;
 		if(a%1==0)
